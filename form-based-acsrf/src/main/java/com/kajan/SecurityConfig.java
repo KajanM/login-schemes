22f30dc647324228ad27da9525116cc55bb046e1
kajan <kajan.14@cse.mrt.ac.lk>
2018-05-28 22:23:30 +0530

    test app for form based authentication scheme with acsrf token

---
+++ form-based-acsrf/src/main/java/com/kajan/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.kajan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	//secret123
+	private static final String ENCODED_PASSWORD = "$2a$10$AIUufK8g6EFhBcumRRV2L.AQNz3Bjp7oDQVFiO5JJMBFZQ6x2/R/2";
+
+	// @formatter:off
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.csrf().and()
+				.authorizeRequests()
+					.antMatchers("/css/**", "/index").permitAll()
+					.antMatchers("/user/**").hasRole("USER")
+					.and()
+				.formLogin().loginPage("/login").failureUrl("/login-error");
+	}
+	// @formatter:on
+
+	// @formatter:off
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth
+			.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("user")
+			.password(ENCODED_PASSWORD)
+			.roles("USER");
+
+	}
+	// @formatter:on
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+}
